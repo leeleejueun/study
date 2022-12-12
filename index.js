d81515452070453d3bef35d7051187fb90537881
leeleejueun <118143272+leeleejueun@users.noreply.github.com>
2022-12-12 18:26:11 +0900

    12/12

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 $('body').prepend('<header>');
 $('body').append('<footer>');
 
-$('header').load('./inc.html header');
+$('header').load('./inc.html header',head);
 $('footer').load('./inc.html footer');
+
+let idx = localStorage.idx || 0;
+
+function head(){
+    $('header .header-menu button').eq(idx).addClass('active');
+    $('header .header-menu button').click(function(){
+        let idx = $(this).index();
+        localStorage.idx=idx;
+    })
+}
