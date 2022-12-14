79f3a28eedef087f225a0ecc10cbc1be8407de27
leeleejueun <leejueun413@gmail.com>
2022-12-14 20:35:55 +0900

    modify:index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $('body').prepend('<header>');
 $('body').append('<footer>');
 
-$('header').load('./inc.html header',head);
-$('footer').load('./inc.html footer');
+$('header').load('./inc.html header>div');
+$('footer').load('./inc.html footer>div');
 
 let idx = localStorage.idx || 0;
 
